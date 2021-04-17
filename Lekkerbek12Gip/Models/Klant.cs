ed3996715ff4f9ffabed665c985a71a1d7672f78
Mozbulut <mstfzblt@gmail.com>
2021-04-17 15:01:18 +0200

    Data notations and syntaxes fixed

--- Lekkerbek12Gip/Models/Klant.cs
+++ Lekkerbek12Gip/Models/Klant.cs
@@ -10,13 +10,17 @@ namespace Lekkerbek12Gip.Models
     public class Klant
     {
         public int KlantId { get; set; }
-        [Required(ErrorMessage ="Naam is verplicht")]
+        [Required(ErrorMessage = "Naam is verplicht")]
+        [Display(Name = "Naam")]
         public string Name { get; set; }
         public string Adress { get; set; }
-        public int GetrouwheidsScore{ get; set; }
+        public int GetrouwheidsScore { get; set; }
         [AdultBirthdateAttribute]
+        [DataType(DataType.Date)]
         public DateTime Geboortedatum { get; set; }
         [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Adress")]
         public string emailadres { get; set; }
 
         public virtual ICollection<Bestelling> Bestellings { get; set; }
